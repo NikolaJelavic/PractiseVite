0be6948533fcfdc785eb435de05f706235e915af
NikolaJelavic <jelavicnikola@gmail.com>
2023-09-29 01:13:02 +0200

    Mongoose

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-// server.mjs
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import express from "express";
+const express = require("express");
+import path from "path";
+import ejs from "ejs";
+import redditData from "./data.json" assert { type: "json" };
 
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import express from 'express';
-import path from 'path';
-// import ejs from 'ejs';
-import redditData from './data.json' assert { type: 'json' };
+// Create a function to connect to MongoDB
+const connectToMongoDB = async () => {
+  try {
+    // Use await to ensure that the connection is established before continuing
+    await mongoose.connect("mongodb://127.0.0.1:27017/MovieApp");
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+};
+
+// Call the function to connect to MongoDB
+connectToMongoDB();
 
 // const redditData = require('./data.json')
 const __filename = fileURLToPath(import.meta.url);
@@ -14,50 +27,50 @@ console.log(redditData);
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.get('/ejs', (req, res) => {
-  res.render('home.ejs');
+app.get("/ejs", (req, res) => {
+  res.render("home.ejs");
 });
 
-app.get('/r/:subreddit', (req, res) => {
+app.get("/r/:subreddit", (req, res) => {
   const { subreddit } = req.params;
   const data = redditData[subreddit];
   const name = subreddit;
   const description = data.description;
-  const subscribers = data.subscribers; 
-  const posts = data.posts; 
+  const subscribers = data.subscribers;
+  const posts = data.posts;
 
-  res.render('subreddit', { name, description, subscribers, posts }); 
+  res.render("subreddit", { name, description, subscribers, posts });
 });
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
- 
+const mongoose = require("mongoose");
+const express = require("express");
 
+const movieSchema = new mongoose.Schema({
+  title: String,
+  year: Number,
+  score: Number,
+  rating: String,
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const Movie = mongoose.model("Movie", movieSchema);
+const amadeus = new Movie({
+  title: "Amadeus",
+  year: 1986,
+  score: 9.2,
+  ratings: "R",
+});
 
 // import { franc } from 'franc';
 // import langs from 'langs';
 // import colors from 'colors';
- 
+
 // const input = process.argv[2];
 // const langCode = franc(input);
 // if (langCode === 'und') {
