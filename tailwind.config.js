4af4dda03a8c4cce2940faaa875e8a60e82aa45f
NikolaJelavic <jelavicnikola@gmail.com>
2023-11-06 19:43:16 +0100

    classnames

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{js,jsx}',
+    './src/**/*.{js,jsx,ts,tsx}',
     './public/index.html',
   ],
   theme: {
