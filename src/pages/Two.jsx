3cae8b28e1504bcf1c040887d47d617382965f2f
NikolaJelavic <jelavicnikola@gmail.com>
2023-11-06 21:17:30 +0100

    icons

--- src/pages/Two.jsx
+++ src/pages/Two.jsx
@@ -1,47 +1,64 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-import ToDo from '../components/ToDo';
-import NavbarTodo from '../components/NavbarTodo';
-import Button from '../components/Button/Button';
+import ToDo from "../components/ToDo";
+import NavbarTodo from "../components/NavbarTodo";
+import Button from "../components/Button/Button";
+
+import { GoBell  } from "react-icons/go";
+// import { GoCloudDownload, GoDatabase } from "react-icons/go";
+
 
 export default function Two() {
-    const [currentTime, setCurrentTime] = useState(new Date());
-    useEffect(() => {
-        // Update the current time every second
-        const intervalId = setInterval(() => {
-          setCurrentTime(new Date());
-        }, 1000);
-    
-        // Clean up the interval on unmount
-        return () => clearInterval(intervalId);
-      }, []);
-    
-      const formattedTime = currentTime.toLocaleTimeString();
-    return <div>
-    <h1>Clock App</h1>
-    <p>Current Time: {formattedTime}</p>
+  const [currentTime, setCurrentTime] = useState(new Date());
+  useEffect(() => {
+    // Update the current time every second
+    const intervalId = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
 
-    
-    <NavbarTodo/>
-    <ToDo />
+    // Clean up the interval on unmount
+    return () => clearInterval(intervalId);
+  }, []);
 
 
+  const handleClick=()=>{
+    console.log('Click');
+  }
+  const formattedTime = currentTime.toLocaleTimeString();
+  return (
     <div>
+      <h1>Clock App</h1>
+      <p>Current Time: {formattedTime}</p>
+
+      <NavbarTodo />
+      <ToDo />
+
       <div>
-        <Button success rounded>Click me</Button>
-      </div>
-      <div>
-        <Button danger>Buy now</Button>
-      </div>
-      <div>
-        <Button warning> 3</Button>
-      </div>
-      <div>
-        <Button secondary>4</Button>
-      </div>
-      <div>
-        <Button primary>5</Button>
+        <div>
+          <Button success outline rounded onClick={handleClick} >
+            <GoBell />
+            Click me
+          </Button>
+        </div>
+        <div>
+          <Button danger outline >
+            {/* <GoCloudDownload /> */}
+            Buy now
+          </Button>
+        </div>
+        <div>
+          <Button warning outline >
+            {/* <GoDatabase /> */}
+            See Deal
+          </Button>
+        </div>
+        <div>
+          <Button secondary>Hide Ads</Button>
+        </div>
+        <div>
+          <Button primary>Something</Button>
+        </div>
       </div>
     </div>
-  </div>
-};
+  );
+}
