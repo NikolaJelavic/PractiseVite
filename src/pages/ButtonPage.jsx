c4fe093887763df4349e2dd6128c0b1af8ba7437
NikolaJelavic <jelavicnikola@gmail.com>
2023-11-12 19:53:01 +0100

    Sunday.

--- src/pages/ButtonPage.jsx
+++ src/pages/ButtonPage.jsx
@@ -1,40 +1,57 @@
-
-
-import { GoBell  } from "react-icons/go";
+import { GoBell } from "react-icons/go";
 // import { GoCloudDownload, GoDatabase } from "react-icons/go";
 
 import Button from "../components/Button/Button";
+import Accordion from "../components/Button/Accordion";
 
+export default function ButtonPage() {
+  const items = [
+    {id:1,
+      label: "Can I use React on a project",
+      content: "You can use React on any project you want",
+    },
+    {id:2,
+      label: "Can I use JS on a project",
+      content: "You can use JS on any project you want",
+    },
+    {id:3,
+      label: "Can I use CSS on a project",
+      content: "You can use CSS on any project you want",
+    },
+  ];
 
-export default function ButtonPage(params) {
-     
-    const handleClick=()=>{
-        console.log('Click');
-      }
-    return <div>
+  const handleClick = () => {
+    console.log("Click");
+  };
+  return (
     <div>
-      <Button success outline rounded onClick={handleClick} >
-        <GoBell />
-        Click me
-      </Button>
+      <div>
+        <Button success outline rounded onClick={handleClick}>
+          <GoBell />
+          Click me
+        </Button>
+      </div>
+      <div>
+        <Button danger outline>
+          {/* <GoCloudDownload /> */}
+          Buy now
+        </Button>
+      </div>
+      <div>
+        <Button warning outline>
+          {/* <GoDatabase /> */}
+          See Deal
+        </Button>
+      </div>
+      <div>
+        <Button secondary>Hide Ads</Button>
+      </div>
+      <div>
+        <Button primary>Something</Button>
+      </div>
+      <div className="m-5 border w-1/3">
+        <Accordion items={items} />
+      </div>
     </div>
-    <div>
-      <Button danger outline >
-        {/* <GoCloudDownload /> */}
-        Buy now
-      </Button>
-    </div>
-    <div>
-      <Button warning outline >
-        {/* <GoDatabase /> */}
-        See Deal
-      </Button>
-    </div>
-    <div>
-      <Button secondary>Hide Ads</Button>
-    </div>
-    <div>
-      <Button primary>Something</Button>
-    </div>
-  </div>
-};
+  );
+}
