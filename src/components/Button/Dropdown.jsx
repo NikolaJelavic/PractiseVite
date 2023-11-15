a917bef9629e3bb49e15fc4f3e9e26cd4823a6fe
NikolaJelavic <jelavicnikola@gmail.com>
2023-11-15 10:11:19 +0100

    working

--- src/components/Button/Dropdown.jsx
+++ src/components/Button/Dropdown.jsx
@@ -1,7 +1,23 @@
-
+import { useState } from "react"
 
 export default function Dropdown({options}) {
-    
+    const [isOpen,setIsOpen]=useState(false);
 
-    return <div>Drodown</div>
+    const handleClick = ()=>{
+        setIsOpen((!isOpen));
+    }
+
+    const handleOptionClick = (option) => {
+        setIsOpen(false)
+        console.log(option);
+    }
+
+    const renderedOptions = options.map((option)=>{
+        return <div onClick={()=>handleOptionClick(option)} key={option.value}>{option.label}</div>
+    })
+
+    return <div className="m-8">
+        <div onClick={handleClick}>Select</div>
+        {isOpen && <div>{renderedOptions}</div>}
+    </div>
 };
