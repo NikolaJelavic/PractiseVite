0916464e461bf6d888f0822069877ef7bbae6873
NikolaJelavic <jelavicnikola@gmail.com>
2023-12-15 11:35:21 +0100

    Jest

--- src/pages/Jest.jsx
+++ src/pages/Jest.jsx
@@ -1,8 +1,20 @@
 import UserForm from "../components/Jest/UserForm";
+import UserList from "../components/Jest/UserList";
+import { useState } from 'react';
 
 
 export default function Jest() {
+    const [users, setUsers] = useState([]);
 
-    return <UserForm/>
-    
+    const onUserAdd = (user) => {
+      setUsers([...users, user]);
+    };
+  
+    return (
+      <div>
+        <UserForm onUserAdd={onUserAdd} />
+        <hr />
+        <UserList users={users} />
+      </div>
+    );
 }
